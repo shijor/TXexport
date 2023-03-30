18800581a3eb4e10d55a1a16534a1b9a0c953f69
shijor <shijor@gmail.com>
2023-03-30 22:02:36 +0800

    initial version

---
+++ src/me/technoholic/BookmarkCreator.java
@@ -0,0 +1,85 @@
+package me.technoholic;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class BookmarkCreator {
+
+   public static void main(String[] args) {
+
+      ArrayList<String> bookmarks = new ArrayList<String>();
+      bookmarks.add("Category 1|Bookmark 1|https://www.bookmark1.com|https://www.bookmark1.com/icon.png");
+      bookmarks.add("Category 2|Bookmark 2|https://www.bookmark2.com|https://www.bookmark2.com/icon.png");
+      bookmarks.add("Category 1|Bookmark 3|https://www.bookmark3.com|https://www.bookmark3.com/icon.png");
+      bookmarks.add("Category 2|Bookmark 4|https://www.bookmark4.com|https://www.bookmark4.com/icon.png");
+      bookmarks.add("Category 3|Bookmark 5|https://www.bookmark5.com|https://www.bookmark5.com/icon.png");
+
+   }
+   
+   
+   public static void createBookmark(ArrayList<String> bookmarks) {
+
+	      // Create a map of categories and their corresponding bookmarks
+	      Map<String, ArrayList<String>> categoryBookmarks = new HashMap<String, ArrayList<String>>();
+	      Iterator<String> iterator = bookmarks.iterator();
+	      while (iterator.hasNext()) {
+	         String bookmark = iterator.next();
+	         String[] parts = bookmark.split("\\|");
+	         String category = "";
+	         String bookmarkName = "";
+	         String url = "";
+	         String favIconUrl = "";
+	         //System.out.println(parts.length);
+	         if (parts.length > 3) {
+	        	 category = parts[0];
+		         bookmarkName = parts[1];
+		         url = parts[2];
+	        	 favIconUrl = parts[3];
+	         }
+	       
+	         if (!categoryBookmarks.containsKey(category)) {
+	            categoryBookmarks.put(category, new ArrayList<String>());
+	         }
+	         categoryBookmarks.get(category).add("<DT><A HREF=\"" + url + "\" ICON=\"" + favIconUrl + "\">" + bookmarkName + "</A>");
+	      }
+
+	      // Generate the bookmark file content
+	      StringBuilder sb = new StringBuilder();
+	      sb.append("<!DOCTYPE NETSCAPE-Bookmark-file-1>\n");
+	      sb.append("<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">\n");
+	      sb.append("<TITLE>Bookmarks</TITLE>\n");
+	      sb.append("<H1>Bookmarks</H1>\n");
+	      sb.append("<DL><p>\n");
+
+	      Iterator<Map.Entry<String, ArrayList<String>>> entries = categoryBookmarks.entrySet().iterator();
+	      while (entries.hasNext()) {
+	         Map.Entry<String, ArrayList<String>> entry = entries.next();
+	         String category = entry.getKey();
+	         ArrayList<String> bookmarksList = entry.getValue();
+
+	         sb.append("<DT><H3 FOLDED>" + category + "</H3>\n");
+	         sb.append("<DL><p>\n");
+
+	         for (String bookmark : bookmarksList) {
+	            sb.append(bookmark + "\n");
+	         }
+
+	         sb.append("</DL><p>\n");
+	      }
+
+	      sb.append("</DL><p>\n");
+
+	      // Write the content to a file
+	      try {
+	         FileWriter fileWriter = new FileWriter("bookmarks.html");
+	         fileWriter.write(sb.toString());
+	         fileWriter.close();
+	         System.out.println("Bookmark file created successfully!");
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+   }
+}
---
+++ src/me/technoholic/TabExtendExport.java
@@ -0,0 +1,84 @@
+package me.technoholic;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class TabExtendExport {
+
+   public static void main(String[] args) {
+
+      JSONParser parser = new JSONParser();
+      BookmarkCreator bmcreateor = new BookmarkCreator();
+	  ArrayList<String> bookmarks = new ArrayList<String>();
+      try {
+
+         // Read the JSON file into a JSON array
+          // Get the current folder path
+          String currentFolderPath = System.getProperty("user.dir");
+
+          // Find the first JSON file in the current folder
+          File[] files = new File(currentFolderPath).listFiles();
+          File jsonFile = null;
+          for (File file : files) {
+             if (file.isFile() && file.getName().endsWith(".json")) {
+                jsonFile = file;
+                break;
+             }
+          }
+          
+          Object obj = parser.parse(new FileReader(jsonFile));
+         //Object obj = parser.parse(new FileReader("file.json"));
+         JSONArray jsonArray = (JSONArray) obj;
+        
+		  System.out.println("Exporting bookmarks..");
+         // Loop through each element in the JSON array
+         for(int i = 0; i < jsonArray.size(); i++) {
+        	 System.out.print(".>");
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+            // Extract the value for the "title" field
+            String category = (String) jsonObject.get("title");
+            //System.out.println("Title: " + category);
+
+            // Extract the values for the "title" and "url" fields in "tabs"
+            JSONArray tabsArray = (JSONArray) jsonObject.get("tabs");
+            for(int j = 0; j < tabsArray.size(); j++) {
+               JSONObject tabObject = (JSONObject) tabsArray.get(j);
+               String tabTitle = (String) tabObject.get("title");
+               String tabUrl = (String) tabObject.get("url");
+               String tabfavIcon = (String) tabObject.get("favIcon");
+             //  System.out.println("Tab Title: " + tabTitle);
+             //  System.out.println("Tab URL: " + tabUrl);
+               if(tabTitle!=null) {
+            	   bookmarks.add(category+"|"+tabTitle+"|"+tabUrl+"|"+tabfavIcon);
+               }
+
+               // Extract the values for the "title" and "url" fields in "stackedItems"
+               if(tabObject.containsKey("stackedItems")) {
+                  JSONArray stackedItemsArray = (JSONArray) tabObject.get("stackedItems");
+                  for(int k = 0; k < stackedItemsArray.size(); k++) {
+                     JSONObject stackedItemObject = (JSONObject) stackedItemsArray.get(k);
+                     String stackedItemTitle = (String) stackedItemObject.get("title");
+                     String stackedItemUrl = (String) stackedItemObject.get("url");
+                     String stackedfavIcon = (String) tabObject.get("favIcon");
+                     //System.out.println("Stacked Item Title: " + stackedItemTitle);
+                   //  System.out.println("Stacked Item URL: " + stackedItemUrl);
+                     if(stackedItemTitle!=null) {
+                    	 bookmarks.add(category+"|"+stackedItemTitle+"|"+stackedItemUrl+"|"+stackedfavIcon);
+                     }
+                  }
+               }
+            }
+         }
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      bmcreateor.createBookmark(bookmarks);
+   }
+}
